d0796f69f862e76a223f0a78cebfae585794dc2a
MRafizeldi <mrafizeldi@gmail.com>
2017-02-05 03:42:27 +0700

    Update plaineriq.debug.js

--- js/plaineriq.debug.js
+++ js/plaineriq.debug.js
@@ -1,4 +1,5 @@
 /* Debug Template */
+console.log('DEBUG');
 console.log('BLOG_ID' ,BLOG_ID);
 if(POST_ID !== '') { console.log('POST_ID' ,POST_ID); }
 if(PAGE_ID !== '') { console.log('PAGE_ID' ,PAGE_ID); }
