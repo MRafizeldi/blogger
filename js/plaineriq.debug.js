dba22cfb490d52fedf858624d032ab1fb7feb6a4
MRafizeldi <mrafizeldi@gmail.com>
2017-02-05 03:57:30 +0700

    Update plaineriq.debug.js

--- js/plaineriq.debug.js
+++ js/plaineriq.debug.js
@@ -1,5 +1,5 @@
 /* Debug Template */
-console.log('DEBUG');
+console.log('DEBUG', DEBUG_ID);
 console.log('BLOG_ID' ,BLOG_ID);
 $( ".post-body .separator:first" ).css( "border", "4px solid #000" ).after('<' + 'ins class="adsbygoogle" style="display:inline-block;width:468px;height:60px;margin:.75em auto .5em;" data-ad-client="ca-pub-7429468915786795" data-ad-slot="8610482312"><' + '/ins><'+'script>(adsbygoogle = window.adsbygoogle || []).push({});<'+'/script>');
 if(POST_ID !== '') { console.log('POST_ID' ,POST_ID); }
