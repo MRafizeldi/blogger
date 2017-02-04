79a6af795ad1f189f9aa7ed54879c7e6cf278d1e
MRafizeldi <mrafizeldi@gmail.com>
2017-02-05 04:05:11 +0700

    Update plaineriq.debug.js

--- js/plaineriq.debug.js
+++ js/plaineriq.debug.js
@@ -1,7 +1,7 @@
 /* Debug Template */
 console.log('DEBUG', DEBUG_ID);
 console.log('BLOG_ID' ,BLOG_ID);
-$( ".post-body .separator:first" ).css( "border", "4px solid #000" ).after('<' + 'ins class="adsbygoogle" style="display:inline-block;width:468px;height:60px;margin:.75em auto .5em;" data-ad-client="ca-pub-7429468915786795" data-ad-slot="8610482312"><' + '/ins><'+'script>(adsbygoogle = window.adsbygoogle || []).push({});<'+'/script>');
+$( ".post-body .separator:first, .pg-index .post-outer:first" ).after('<' + 'ins class="adsbygoogle" style="display:inline-block;width:468px;height:60px;margin:.75em auto .5em;" data-ad-client="ca-pub-7429468915786795" data-ad-slot="8610482312"><' + '/ins><'+'script>(adsbygoogle = window.adsbygoogle || []).push({});<'+'/script>');
 if(POST_ID !== '') { console.log('POST_ID' ,POST_ID); }
 if(PAGE_ID !== '') { console.log('PAGE_ID' ,PAGE_ID); }
 $('.adsbygoogle').each(function(i,val) {
