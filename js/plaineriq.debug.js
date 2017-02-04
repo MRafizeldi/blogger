70f1acbdc872b79ebaa75e4fedc06187936c4daa
MRafizeldi <mrafizeldi@gmail.com>
2017-02-05 03:35:01 +0700

    Create plaineriq.debug.js

---
+++ js/plaineriq.debug.js
@@ -0,0 +1,9 @@
+/* Debug Template */
+console.log('BLOG_ID' ,BLOG_ID);
+if(POST_ID !== '') { console.log('POST_ID' ,POST_ID); }
+if(PAGE_ID !== '') { console.log('PAGE_ID' ,PAGE_ID); }
+$('.adsbygoogle').each(function(i,val) {
+var adclient = $(this).data('ad-client');
+var adslot = $(this).data('ad-slot');
+console.log('adclient = ' + adclient + ', adslot = ' + adslot);
+});
