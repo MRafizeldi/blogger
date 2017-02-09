1d64ec03f54ac47a4574b77468654047884c4b75
MRafizeldi <mrafizeldi@gmail.com>
2017-02-09 18:47:31 +0700

    Update advertisement.js

--- js/advertisement.js
+++ js/advertisement.js
@@ -1,8 +1,9 @@
 jQuery.adblock = false;
 console.log('BLOG_ADSENSE 1',BLOG_ADSENSE);
-if(BLOG_ADSENSE == false) {
-console.log('BLOG_ADSENSE 2',BLOG_ADSENSE);
-}
+if(BLOG_ADSENSE) {
 if(BLOG_ADSENSE == true) {
+console.log('BLOG_ADSENSE 2',BLOG_ADSENSE);
+} else {
 console.log('BLOG_ADSENSE 3',BLOG_ADSENSE);
+}
 }
