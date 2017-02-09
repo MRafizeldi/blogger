b1ee5ba5febfae2b630bc7aad20e2e2eae242797
MRafizeldi <mrafizeldi@gmail.com>
2017-02-09 18:41:09 +0700

    Create advertisement.js

---
+++ js/advertisement.js
@@ -0,0 +1,8 @@
+jQuery.adblock = false;
+console.log('BLOG_ADSENSE 1',BLOG_ADSENSE);
+if(BLOG_ADSENSE == false) {
+console.log('BLOG_ADSENSE 2',BLOG_ADSENSE);
+}
+if(BLOG_ADSENSE == true) {
+console.log('BLOG_ADSENSE 3',BLOG_ADSENSE);
+}
